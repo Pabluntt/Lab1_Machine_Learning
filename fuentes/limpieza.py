5179d1381fa73252b3828cd1a2bfb45ff7d98e64
Pabluntt <pablo.lopez.saavedra3001@gmail.com>
2026-04-26 11:55:08 -0400

    COSAS NUEVAS

--- fuentes/limpieza.py
+++ fuentes/limpieza.py
@@ -1,4 +1,5 @@
 import re
+import html
 import nltk
 from nltk.corpus import stopwords
 
@@ -15,16 +16,65 @@ def _cargar_stopwords_es():
 
 STOPWORDS_ES = _cargar_stopwords_es()
 
+RUIDO_HTML = {
+    "strong",
+    "em",
+    "div",
+    "span",
+    "link",
+    "href",
+    "rel",
+    "target",
+    "blank",
+    "class",
+    "style",
+    "title",
+    "html",
+    "body",
+    "head",
+    "meta",
+    "script",
+    "iframe",
+    "blockquote",
+    "br",
+    "li",
+    "ul",
+    "ol",
+    "svg",
+    "path",
+    "viewbox",
+    "nbsp",
+    "quot",
+    "amp",
+    "lt",
+    "gt",
+    "src",
+    "img",
+    "http",
+    "https",
+    "www",
+    "com",
+}
+
 def limpiar_texto(texto):
     if texto is None:
         texto = ""
 
+    texto = html.unescape(str(texto))
     texto = texto.lower()
+    texto = re.sub(r"<[^>]+>", " ", texto)
+    texto = re.sub(r"&[a-zA-Z]+;", " ", texto)
     texto = re.sub(r"http\S+", " ", texto)
     texto = re.sub(r"@\w+", " ", texto)
     texto = re.sub(r"#", " ", texto)
     texto = re.sub(r"[^a-zA-Záéíóúñü\s]", " ", texto)
 
-    tokens = [t for t in texto.split() if t not in STOPWORDS_ES]
+    tokens = [
+        t
+        for t in texto.split()
+        if t not in STOPWORDS_ES
+        and t not in RUIDO_HTML
+        and len(t) > 2
+    ]
 
     return " ".join(tokens)
